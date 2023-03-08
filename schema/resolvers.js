053907280e98d375d2be0720efb857208f456ae3
umair2607 <umair.k2607@gmail.com>
2023-03-08 17:29:57 +0500

    apollo 🚀 server setup

---
+++ schema/resolvers.js
@@ -0,0 +1,11 @@
+const {UserList} = require("../FakeData")
+
+const resolvers = {
+    Query: {
+        users()  {
+            return UserList;
+        }
+    }
+}
+
+module.exports = {resolvers};
