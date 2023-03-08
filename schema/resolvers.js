21f9c8bd7fb19b7d31eb9c8989f6829d0e30eaba
umair2607 <umair.k2607@gmail.com>
2023-03-08 18:06:38 +0500

    single 👦 user query + friends query

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,15 @@
 const {UserList} = require("../FakeData")
+const _ = require("lodash");
 
 const resolvers = {
     Query: {
         users()  {
             return UserList;
+        },
+        user(parent,args){
+            const id = args.id;
+            const user = _.find(UserList,{id: Number(id)})
+            return user;
         }
     }
 }
